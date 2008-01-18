da83b1d569d3b9ae6d227b15680ccba1fba6c533
swartz@pobox.com <swartz@pobox.com@fe4db14e-1138-0410-ae3a-bd28db37d31a>
2008-01-18 17:54:34 +0000

    add internal_only and required_modules options for test classes
    git-svn-id: https://perl-cache.googlecode.com/svn/chi/trunk@295 fe4db14e-1138-0410-ae3a-bd28db37d31a

--- lib/CHI/Test/Class.pm
+++ lib/CHI/Test/Class.pm
@@ -1,9 +1,11 @@
 package CHI::Test::Class;
 use strict;
 use warnings;
-use CHI::Util qw(require_dynamic);
+use CHI::Util qw(dump_one_line require_dynamic);
 use Getopt::Long;
 use Module::Find qw(findallmod);
+use Module::Load::Conditional qw(can_load);
+use Test::More qw();
 use base qw(Test::Class);
 
 sub load_tests {
@@ -38,10 +40,46 @@ sub runtests {
     GetOptions( 'm|method=s' => sub { $ENV{TEST_METHOD} = ".*" . $_[1] . ".*" },
     );
 
+    # Check for internal_only
+    #
+    if ($class->internal_only && !$class->is_internal) {
+        $class->skip_all('internal test only');
+    }
+
+    # Check for required modules
+    #
+    if (my $required_modules = $class->required_modules) {
+        unless (can_load(modules => $required_modules)) {
+            $class->skip_all(sprintf('one of required modules not installed: %s', dump_one_line($required_modules)));
+        }
+    }
+
     # Only run tests directly in $class.
     #
     my $test_obj = $class->new();
     Test::Class::runtests($test_obj);
+}
+
+sub skip_all
+{
+    my ($reason) = @_;
+    
+    Test::More::plan(skip_all => $reason);
+}
+
+sub is_internal
+{
+    return $ENV{CHI_INTERNAL_TESTS};
+}
+
+sub internal_only
+{
+    return 0;
+}
+
+sub required_modules
+{
+    return {};
 }
 
 1;
