5090719452cc2f1fd14b81f2ac76e704a9244cea
swartz@pobox.com <swartz@pobox.com@fe4db14e-1138-0410-ae3a-bd28db37d31a>
2008-01-18 17:53:57 +0000

    added
    git-svn-id: https://perl-cache.googlecode.com/svn/chi/trunk@294 fe4db14e-1138-0410-ae3a-bd28db37d31a

---
+++ lib/CHI/t/RequiredModules.pm
@@ -0,0 +1,16 @@
+package CHI::t::RequiredModules;
+use strict;
+use warnings;
+use CHI::Test;
+use base qw(CHI::Test::Class);
+
+sub internal_only { 1 }
+
+sub required_modules { return { 'Data::Dumper' => undef, 'blarg' => undef } };
+
+sub test_blarg : Test(1) {
+    require Blarg;
+    Blarg->funny();
+}
+
+1;
---
+++ t/RequiredModules.t
@@ -0,0 +1,5 @@
+#!perl -w
+use strict;
+use warnings;
+use CHI::t::RequiredModules;
+CHI::t::RequiredModules->runtests;
