175c6721147b69920def1a740525c60ae05f679d
swartz@pobox.com <swartz@pobox.com@fe4db14e-1138-0410-ae3a-bd28db37d31a>
2008-01-19 23:53:05 +0000

    use temp dir rather than default dir, which is unreliable for testing
    git-svn-id: https://perl-cache.googlecode.com/svn/chi/trunk@327 fe4db14e-1138-0410-ae3a-bd28db37d31a

--- lib/CHI/t/Driver/Multilevel/Single.pm
+++ lib/CHI/t/Driver/Multilevel/Single.pm
@@ -2,17 +2,22 @@ package CHI::t::Driver::Multilevel::Single;
 use strict;
 use warnings;
 use CHI::Test;
+use File::Temp qw(tempdir);
 use base qw(CHI::t::Driver);
 
 # Test multilevel driver with a single subcache.
 
+my $root_dir;
+
 sub new_cache_options {
     my $self = shift;
 
+    $root_dir ||=
+      tempdir( "chi-driver-multilevel-XXXX", TMPDIR => 1, CLEANUP => 1 );
     return (
         $self->SUPER::new_cache_options(),
         driver    => 'Multilevel',
-        subcaches => [ { driver => 'File' } ]
+        subcaches => [ { driver => 'File', root_dir => $root_dir } ]
     );
 }
 
