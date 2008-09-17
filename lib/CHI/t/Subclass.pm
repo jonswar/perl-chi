64d6e4e68a34aabed00a3af43593c98496894f7c
swartz@pobox.com <swartz@pobox.com@fe4db14e-1138-0410-ae3a-bd28db37d31a>
2008-09-17 03:35:37 +0000

    added
    git-svn-id: https://perl-cache.googlecode.com/svn/chi/trunk@476 fe4db14e-1138-0410-ae3a-bd28db37d31a

---
+++ lib/CHI/t/Subclass.pm
@@ -0,0 +1,29 @@
+package CHI::t::Subclass;
+use strict;
+use warnings;
+use CHI::Test;
+use base qw(CHI::Test::Class);
+
+# Test declare_unsupported_methods
+#
+{
+
+    package CHI::t::Subclass::Driver::HasUnsupported;
+    use Moose;
+    extends 'CHI::Driver::Memory';
+    __PACKAGE__->declare_unsupported_methods(qw(get_namespaces));
+    __PACKAGE__->meta->make_immutable;
+}
+
+sub test_unsupported : Tests(2) {
+    my $cache =
+      CHI->new( driver_class => 'CHI::t::Subclass::Driver::HasUnsupported' );
+    lives_ok( sub { $cache->get_keys }, 'get_keys lives' );
+    throws_ok(
+        sub { $cache->get_namespaces },
+        qr/method 'get_namespaces' not supported by 'CHI::t::Subclass::Driver::HasUnsupported'/,
+        'get_namespaces dies'
+    );
+}
+
+1;
---
+++ t/Subclass.t
@@ -0,0 +1,6 @@
+#!perl -w
+use strict;
+use warnings;
+use lib 't/lib';
+use CHI::t::Subclass;
+CHI::t::Subclass->runtests;
