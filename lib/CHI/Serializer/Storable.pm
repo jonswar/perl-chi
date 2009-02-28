df69099baf31b3d3abd4058eb086f4e91454cad5
Jonathan Swartz <swartz@pobox.com>
2009-02-27 17:37:37 -0800

    eliminate required dependency on Data::Serializer

---
+++ lib/CHI/Serializer/Storable.pm
@@ -0,0 +1,18 @@
+# Default serializer class, so that we don't have to depend on Data::Serializer.
+# Recommend use Data::Serializer for other serializers.
+#
+package CHI::Serializer::Storable;
+use Mouse;
+use Storable;
+use strict;
+use warnings;
+
+sub serialize {
+    return Storable::freeze( $_[1] );
+}
+
+sub deserialize {
+    return Storable::thaw( $_[1] );
+}
+
+1;
