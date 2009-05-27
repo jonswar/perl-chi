444a1c32991ee2876a62ce8d0f81255b732b6a4e
Jonathan Swartz <swartz@pobox.com>
2009-05-27 15:31:42 -0700

    non-moose drivers should work now, since we moved away from applying role to attribute

--- lib/CHI/t/Driver/NonMoose.pm
+++ lib/CHI/t/Driver/NonMoose.pm
@@ -4,24 +4,7 @@ use warnings;
 use CHI::Test;
 use base qw(CHI::t::Driver::Memory);
 
-sub testing_driver_class { 'CHI::Test::Driver::NonMoose' }
-
-# These won't work without Moose
-sub test_custom_discard_policy         { }
-sub test_discard_timeout               { }
-sub test_l1_cache                      { }
-sub test_lru_discard                   { }
-sub test_max_size                      { }
-sub test_mirror_cache                  { }
-sub test_size_awareness                { }
-sub test_size_awareness_with_subcaches { }
-sub test_short_driver_name             { }
-
-sub test_apply_role : Tests(1) {
-    my ($self) = @_;
-
-    throws_ok { $self->new_cache( max_size => 100 ) }
-    qr/cannot apply role to non-Moose driver/;
-}
+sub testing_driver_class   { 'CHI::Test::Driver::NonMoose' }
+sub test_short_driver_name { }
 
 1;
