39b13c6108f96fce7deeeac9baa3ed5780197b81
Jonathan Swartz <swartz@pobox.com>
2009-06-02 13:43:02 -0700

    fix spacing

--- lib/CHI.pm
+++ lib/CHI.pm
@@ -1073,9 +1073,7 @@ CHI is provided "as is" and without any express or implied warranties,
 including, without limitation, the implied warranties of merchantibility and
 fitness for a particular purpose.
 
-This program is free software; you can redistribute
-
- it and/or modify it under
+This program is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
 
 =cut
