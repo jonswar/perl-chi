48f48a3b3b2489be18e7ba24cd442981be94fb4c
Jonathan Swartz <swartz@pobox.com>
2009-07-10 12:14:30 -0700

    changed

--- git/hooks/pre-commit.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use Cwd qw(realpath);
-use File::Basename;
-
-my $root_dir = dirname(dirname(dirname(realpath($0))));
-system("cd $root_dir; /Users/swartz/std/bin/cptools/cptidy");
-if ($?) {
-    die "cptidy failed";
-}
