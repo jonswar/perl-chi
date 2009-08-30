a3887499b0a4c38a498de0ae45117d77ecccc8c4
Jonathan Swartz <swartz@pobox.com>
2009-08-30 15:39:27 -0700

    get rid of unused LOCAL_LIBS block

--- lib/CHI/Test.pm
+++ lib/CHI/Test.pm
@@ -3,26 +3,11 @@
 package CHI::Test;
 use List::MoreUtils qw(uniq);
 use Class::MOP;
-use strict;
-use warnings;
-
-BEGIN {
-
-    # If PERL_LOCAL_LIB is set, run tests with libs restricted to PERL_LOCAL_LIB
-    # OS X specific for now
-    #
-    if ( my $local_lib = $ENV{PERL_LOCAL_LIB} ) {
-        my @new_inc = uniq( grep { !m{^(/Library|/Network/Library)} } @INC );
-        @INC = @new_inc;    ## no critic (RequireLocalizedPunctuationVars)
-        warn "\nUsing local lib '$local_lib'\n";
-        push( @INC,
-            map { "$ENV{PERL_LOCAL_LIB}/lib/perl5$_" }
-              ( "", "/darwin-thread-multi-2level" ) );
-    }
-}
 use CHI;
 use CHI::Driver::Memory;
 use CHI::Test::Logger;
+use strict;
+use warnings;
 
 sub import {
     my $class = shift;
