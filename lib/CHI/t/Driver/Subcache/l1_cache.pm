5702282855789147fbe52c79f45ef6d1f2e16568
Jonathan Swartz <swartz@pobox.com>
2010-02-09 11:01:10 -0800

    add special test_stats for l1_cache

--- lib/CHI/t/Driver/Subcache/l1_cache.pm
+++ lib/CHI/t/Driver/Subcache/l1_cache.pm
@@ -2,6 +2,7 @@ package CHI::t::Driver::Subcache::l1_cache;
 use strict;
 use warnings;
 use CHI::Test;
+use CHI::Test::Util qw(activate_test_logger);
 use File::Temp qw(tempdir);
 use base qw(CHI::t::Driver::Subcache);
 
@@ -21,6 +22,34 @@ sub new_cache_options {
         root_dir => $root_dir,
         l1_cache => { driver => 'Memory', global => 1 },
     );
+}
+
+sub test_stats : Test(3) {
+    my $self = shift;
+
+    my $stats = $self->testing_chi_root_class->stats;
+    $stats->enable();
+
+    my ( $key, $value ) = $self->kvpair();
+    my $start_time = time();
+
+    my $cache;
+    $cache = $self->new_cache( namespace => 'Foo' );
+    $cache->get($key);
+    $cache->set( $key, $value, 80 );
+    $cache->get($key);
+
+    my $log = activate_test_logger();
+    $log->empty_ok();
+    $stats->flush();
+
+    $log->contains_ok(
+        qr/CHI stats: namespace='Foo'; cache='File'; start=.*; end=.*; absent_misses=1; set_key_size=6; set_value_size=20; sets=1/
+    );
+    $log->contains_ok(
+        qr/CHI stats: namespace='Foo'; cache='File:l1_cache'; start=.*; end=.*; absent_misses=1; hits=1/
+    );
+
 }
 
 1;
